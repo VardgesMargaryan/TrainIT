faf18fa5270fedc24833b00861faea67ec94bc48
Vardges <vardges.v.margaryan@gmail.com>
2023-02-20 20:58:05 +0400

    JSON reading a GSON deserialization

--- app/src/main/java/com/example/trainit/ConditionalsActivity.java
+++ app/src/main/java/com/example/trainit/ConditionalsActivity.java
@@ -2,13 +2,58 @@ package com.example.trainit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ConditionalsActivity extends AppCompatActivity {
+    String probJSON;
+    TextView showJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conditionals);
+        showJson = findViewById(R.id.showjson);
+
+
+        Gson gson = new Gson();
+        Problem problem = gson.fromJson(probJSON, Problem.class);
+        Log.i("GSON", "Problem: "+problem.problem +" Answer:"+problem.answer);
+
+
+
+
+    }
+
+    public String loadJSONFromAsset(Context context) {
+         String json;
+        try {
+            InputStream is = context.getAssets().open("ConditionalProblems.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        probJSON = json;
+        return probJSON;
+
     }
 }
---
+++ app/src/main/java/com/example/trainit/Problem.java
@@ -0,0 +1,14 @@
+package com.example.trainit;
+
+public class Problem {
+    public String problem;
+    public String answer;
+
+    public Problem(String problem, String answer) {
+        this.problem = problem;
+        this.answer = answer;
+    }
+
+    public Problem() {
+    }
+}
