7766e63329db084a1628e2fcaffa03d9d1bf7018
Vardges <vardges.v.margaryan@gmail.com>
2023-03-03 18:13:26 +0400

    changing language function added into ProfileActivity

--- app/src/main/java/com/example/trainit/ProfileActivity.java
+++ app/src/main/java/com/example/trainit/ProfileActivity.java
@@ -1,8 +1,13 @@
 package com.example.trainit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,22 +19,51 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Locale;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView logOut;
+    TextView logOut, email, changeLanguage;
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
+    private FirebaseAuth mAuth;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();
         setContentView(R.layout.activity_profile);
 
         logOut = findViewById(R.id.logout_txt);
+        email = findViewById(R.id.email_show_tv);
+        changeLanguage = findViewById(R.id.change_language_tv);
+
+        mAuth = FirebaseAuth.getInstance();
+
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this,gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct != null){
+            String personEmail = acct.getEmail();
+            email.setText(personEmail);
+        }else{
+            String emailShow = mAuth.getCurrentUser().getEmail();
+            email.setText(emailShow);
+
+        }
+
+        changeLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showChangeLanguageDialog();
+
+            }
+        });
 
 
         logOut.setOnClickListener(new View.OnClickListener() {
@@ -49,4 +83,50 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    private void showChangeLanguageDialog() {
+        final String[] languages = {"Русский", "English"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(ProfileActivity.this);
+        mBuilder.setTitle("Choose Language...");
+        mBuilder.setSingleChoiceItems(languages, -1, (dialog, which) -> {
+            if(which == 0){
+                //Russian
+                setLocale("ru");
+                recreate();
+            }
+            else if(which == 1){
+                //English
+                setLocale("en");
+                recreate();
+            }
+
+            dialog.dismiss();
+
+        });
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+    //--
+
+    private void setLocale(String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", language);
+        editor.apply();
+
+    }
+
+    public void loadLocale(){
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String lang = prefs.getString("My_Lang", "");
+        setLocale(lang);
+    }
+
+
 }
